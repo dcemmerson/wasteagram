f066f2fc2e01c419920ed1cd2b9f01d7ffb2992a
dcemmerson <emmersod@oregonstate.edu>
2020-08-14 10:14:16 -0700

    More semantic labels.

--- lib/pages/base/page_base.dart
+++ lib/pages/base/page_base.dart
@@ -32,23 +32,30 @@ abstract class PageBase extends StatelessWidget {
       return Theme(
           data: themeManager.themeData,
           child: Scaffold(
-            appBar: AppBar(
+              appBar: AppBar(
 //              bottom: PreferredSize, //_buildAppBarFoot(),
-              centerTitle: true,
-              elevation: 0.0,
-              title: Text(pageTitle),
-              textTheme: Theme.of(context).primaryTextTheme,
-              actions: [
-                SettingsDrawerIcon(),
-              ],
-            ),
-            endDrawer: SettingsDrawer(),
-            body: body,
-            floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-            floatingActionButton: FloatingActionButton(
-                onPressed: () => Routes.addWastedPost(context),
-                child: Icon(Icons.add_a_photo)),
-          ));
+                centerTitle: true,
+                elevation: 0.0,
+                title: Text(pageTitle),
+                textTheme: Theme.of(context).primaryTextTheme,
+                actions: [
+                  SettingsDrawerIcon(),
+                ],
+              ),
+              endDrawer: SettingsDrawer(),
+              body: body,
+              floatingActionButtonLocation:
+                  FloatingActionButtonLocation.endFloat,
+              floatingActionButton: Semantics(
+                button: true,
+                enabled: true,
+                label: 'New Waste Post',
+                hint: 'Add new waste post',
+                child: FloatingActionButton(
+                    key: Key('addWastePost'),
+                    onPressed: () => Routes.addWastedPost(context),
+                    child: Icon(Icons.add_a_photo)),
+              )));
     } else {
       return Theme(
           data: themeManager.themeData,
